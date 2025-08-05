05e18257e6847f241a879deb2c70ac08d76e92ae
kalifun <kalifun@163.com>
2025-08-05 10:36:26 +0800

    fixed: instant action

--- instant_actions/instant_actions.go
+++ instant_actions/instant_actions.go
@@ -2,8 +2,17 @@ package instant_actions
 
 import "github.com/kalifun/vda5050-types-go"
 
+// InstantAction represents an action to be executed immediately
+type InstantAction struct {
+	ActionType   string               `json:"actionType"`                  // Type of the action
+	ActionId     string               `json:"actionId"`                    // Unique ID of the action
+	BlockingType vda5050.BlockingType `json:"blockingType"`                // How action blocks others
+	Description  *string              `json:"actionDescription,omitempty"` // Optional description
+	Parameters   []ActionParameter    `json:"actionParameters,omitempty"`  // Optional parameters
+}
+
 // InstantActionParameter represents a parameter for an instant action
-type InstantActionParameter struct {
+type ActionParameter struct {
 	Key   string      `json:"key"`   // Parameter key
 	Value interface{} `json:"value"` // Parameter value (can be any JSON type)
 }
@@ -11,5 +20,5 @@ type InstantActionParameter struct {
 // InstantActions represents a message containing instant actions
 type InstantActions struct {
 	vda5050.ProtocolHeader
-	Actions []InstantActionParameter `json:"actions"` // List of actions to execute
+	Actions []InstantAction `json:"actions"` // List of actions to execute
 }
